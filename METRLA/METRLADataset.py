7cbd0575a825d680bd5b5c706df4420d24c20d50
Nishu Choudhary <nishuchoudhary@gatech.edu>
2024-09-27 13:06:21 -0500

    Added methods to add features, targets, edge_indices, and edge_weights

--- METRLA/METRLADataset.py
+++ METRLA/METRLADataset.py
@@ -18,7 +18,7 @@ import numpy as np
 
 import torch
 import torch_geometric
-from torch_geometric.utils import to_dense_adj
+from torch_geometric.utils import dense_to_sparse
 from torch_geometric_temporal.signal import StaticGraphTemporalSignal
 
 # Turning off ssl certification is also an option - not recommended
@@ -46,24 +46,40 @@ class METRLADataset(object):
         node_val_file = "node_values.npy"
 
         metrla_zipfile_path = os.path.join(self.root_dir, metrla_file)
+        adj_filepath = os.path.join(self.root_dir, adjacency_file)
+        nodeval_filepath = os.path.join(self.root_dir, node_val_file)
 
         if not os.path.isfile(metrla_zipfile_path):
             self._download_url(os.path.join(base_url, metrla_file), metrla_zipfile_path)
 
         # --- Check if the METRLA file has been unzipped and the two files are extracted
-        if not os.path.isfile(os.path.join(self.root_dir, adjacency_file)) \
-                or not os.path.isfile(os.path.join(self.root_dir, adjacency_file)):
+        if not os.path.isfile(adj_filepath) or not os.path.isfile(nodeval_filepath):
             # Extract Zipfile
             with zipfile.ZipFile(metrla_zipfile_path, 'r') as zip_fph:
                 zip_fph.extractall(self.root_dir)
 
         # --- Additional Checks if Adjacency and Node Value Numpy files exists
-        assert os.path.isfile(os.path.join(self.root_dir, adjacency_file)), "Adjacency file does not exist"
-        assert os.path.isfile(os.path.join(self.root_dir, node_val_file)), "Node Values file does not exist"
+        assert os.path.isfile(adj_filepath), "Adjacency file does not exist"
+        assert os.path.isfile(nodeval_filepath), "Node Values file does not exist"
 
+        self.A = torch.from_numpy(np.load(adj_filepath))
 
-        self.A = None
-        self.X = None
+        # --- Z-score Normalization Data Pre-processing before assigning as X
+        X = np.load(nodeval_filepath)
+        X = X.astype(np.float32)
+        # X.shape = [34272, 207, 2] => [num_samples, num_nodes, num_features(avg_speed, occupancy_rate)]
+        X = X.transpose(1, 2, 0)
+
+        # X.shape = [207, 2, 34272] => [num_samples, num_nodes, num_features(avg_speed, occupancy_rate)]
+        # --- Estimate mean value of speed and occupancy rate for all nodes (Assumed uniformity) 
+        # --- and entire sample (Data Leak)
+
+        means = np.mean(X, axis=(0, 2)).reshape(1, -1, 1)
+        stds = np.std(X, axis=(0, 2)).reshape(1, -1, 1)
+
+        X = (X-means)
+        X = X / stds
+        self.X = torch.from_numpy(X)
 
 
     def _download_url(self, url, directory):
@@ -74,16 +90,37 @@ class METRLADataset(object):
 
 
     def _get_edges_and_weights(self):
-        self.edge_index = None
-        self.edge_weight = None
+        edge_index, edge_weights = dense_to_sparse(self.A)
+        self.edge_index = edge_index.numpy()  # StaticGraphTemporalSignal Edge_Index = Union[np.ndarray, None]
+        self.edge_weight = edge_weights.numpy() # StaticGraphTemporalSignal, Edge_Weight = Union[np.ndarray, None]
 
-    def _generate_task(self):
-        features = None
-        targets = None
+    def _generate_task(self, num_timesteps_in=12, num_timesteps_out=12):
+        """
+        :param num_timesteps_in: number of historical timesteps model utilizes for a single snapshot (default: 12)
+        :param num_timesteps_out: number of future timesteps model predicts for a single snapshot (default: 12)
+        :return: features: Sequence[Union[np.ndarray, None]], targets: Sequence[Union[np.ndarray, None]]
+        """
+        delta = (num_timesteps_in+num_timesteps_out)
+        # Initialize features and targets as lists
+        features, targets = [], []
+
+        # X.shape = [207, 2, 34272] => [num_samples, num_nodes, num_features(avg_speed, occupancy_rate)]
+        indices = [(i, (i+delta)) for i in range(self.X.shape[2]-(delta)+1)]
+        for i, j in indices:
+
+            features.append((self.X[:, :, i:i+num_timesteps_in]).numpy())
+            # For Targets we only want speed
+            targets.append((self.X[:, 0, i+num_timesteps_in:j]).numpy())
         self.features = features
         self.targets = targets
 
-    def get_dataset(self):
+
+    def get_dataset(self, num_timesteps_in=12, num_timesteps_out=12):
+        """Returns data iterator for METR-LA dataset as an instance of the Static Graph Temporal Signal class.
+        :return: **dataset** *(StaticGraphTemporalSignal)* - The METR-LA traffic forecasting dataset.
+        """
+        self._get_edges_and_weights()
+        self._generate_task(num_timesteps_in, num_timesteps_out)
         dataset = StaticGraphTemporalSignal(self.edge_index, self.edge_weight, self.features, self.targets)
         return dataset
 
